9f36278af4f76640465104afc137d6a5733b481f
Owinnie <99328720+Owinnie@users.noreply.github.com>
2022-10-30 14:13:46 +0300

    3js

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -15,7 +15,7 @@ function start () {
   });
 
   apiStatus();
-  fetchPlaces();
+  searchPlaces();
 }
 
 function apiStatus () {
@@ -29,7 +29,7 @@ function apiStatus () {
   });
 }
 
-function fetchPlaces () {
+function searchPlaces () {
   const PLACES_URL = `http://${HOST}:5001/api/v1/places_search/`;
   $.ajax({
     url: PLACES_URL,
